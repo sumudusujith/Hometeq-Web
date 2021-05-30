0d729a9031ca9bbb6ff6d60081eb6cb0bc2e6d80
sumudusujith <sumudu.2019582@iit.ac.lk>
2021-05-30 18:08:38 +0530

    sql com1

--- db.php
+++ db.php
@@ -1,14 +1,16 @@
 <?php
-$dbhost = 'phpmyadmin.ecs.westminster.ac.uk';
-$dbuser = 'yourWestminsterID';
-$dbpass = 'yourMySQLRandomPwd';
-$dbname = 'yourWestminsterID_0';
+$dbhost = 'localhost';
+$dbuser = 'root';
+$dbpass = '';
+$dbname = 'serverside';
+
+
 //create a DB connection
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 //if the DB connection fails, display an error message and exit
 if (!$conn)
 {
-die('Could not connect: ' . mysqli_error($conn));
+ die('Could not connect: ' . mysqli_error($conn));
 }
 //select the database
 mysqli_select_db($conn, $dbname);
